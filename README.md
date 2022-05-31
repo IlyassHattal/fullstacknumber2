3a9a4ade025b2f52085c5079f8ca095183b620c0
Ilyass Hattal <101954954+IlyassHattal@users.noreply.github.com>
2022-05-30 21:50:26 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+# fs-one-post-its
---
+++ config/database.js
@@ -0,0 +1,7 @@
+// config/database.js
+module.exports = {
+
+  'url': 'mongodb+srv://ilyasshattal:Teddybear123@cluster0.965rl.mongodb.net/cluster0?retryWrites=true&w=majority',
+  //above, postits is name of collection
+  'dbName': 'helloeli'
+};
